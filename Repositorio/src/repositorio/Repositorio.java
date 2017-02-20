95234a17e640d99069ed9f684357ed31b43b28cd
marioherrerac <steven.herrera.cr@gmail.com>
2017-02-20 16:21:20 -0600

    Add files via upload

---
+++ Repositorio/src/repositorio/Repositorio.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositorio;
+
+/**
+ * @author Mario Herrera Carmona
+ */
+public class Repositorio {
+
+    /**
+     * haciendo repositorio de ejemplo
+     * para posibles proyectos a futuro
+     * problemas por los branches pero si aparece
+     * en el repositorio de github
+     * estoy probando solo por ver como lo logro 
+     * poner con master tengo dudas aun con esta
+     * parte, sin embargo no i won't give in 'tll
+     * it's done!!!
+     */
+    public static void main(String[] args) {
+        System.out.print("Hola Mundo!!!!!!!!");
+    }
+    
+}
